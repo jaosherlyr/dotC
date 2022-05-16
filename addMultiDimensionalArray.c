9939a8b065a19e0a07f101a2d4f580495b594bc2
jaosherlyr <70427806+jaosherlyr@users.noreply.github.com>
2022-05-16 13:59:35 +0800

    adding matrices in c

---
+++ addMultiDimensionalArray.c
@@ -0,0 +1,51 @@
+//create a function that will return the sum of two matrices
+#include <stdio.h>
+#include <stdlib.h>
+typedef int MyMatrix[4][3];
+void displayArray(int ** array);
+int ** addTwoDimensional(MyMatrix arrA, MyMatrix arrB);
+int main() {
+    //declare pointer to resulting array
+    int ** result;
+
+    //initialize array
+    MyMatrix A = {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}, {3, 2, 1}};
+    MyMatrix B = {{2, 1, 4}, {6, 3, 1}, {7, 3, 3}, {6, 3, 9}};
+
+    //call function
+    result = addTwoDimensional(A, B);
+    displayArray(result);
+}
+int ** addTwoDimensional(MyMatrix arrA, MyMatrix arrB) {
+    int ** array;
+    int row, col, ctr;
+
+    //allocate memory to the array
+    array = (int **)malloc(4 * sizeof(int *));
+
+    for (ctr = 0; ctr < 4; ctr++) {
+        array[ctr] = (int *)malloc(3 * sizeof(int));
+    }
+
+
+    //get the sums
+    for (row = 0; row < 4; row++) {
+        for (col = 0; col < 3; col++) {
+            array[row][col] = arrA[row][col] + arrB[row][col];
+        }
+    }
+
+    return array;
+}
+void displayArray(int ** array) {
+    int row, col;
+
+    for (row = 0; row < 4; row++) {
+
+        for (col = 0; col < 3; col++) {
+            printf("%d\t", array[row][col]);
+        }
+
+        printf("\n");
+    }
+}
---
+++ magicSquare.c
@@ -0,0 +1,83 @@
+//create a code to chck if the givin multidimensional array is a Magic square
+#include <stdio.h>
+#define SIZE 3
+int magicSquare(int square[SIZE][SIZE]);
+int main () {
+    int Magic[SIZE][SIZE] = {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}};
+    int temp;
+
+    temp = magicSquare(Magic);
+
+    //print resut
+    printf("The square is %s MAGIC\n", (temp == 0) ? "is" : "is NOT");
+}
+int magicSquare(int square[SIZE][SIZE]) {
+    int magic = 0, row, col, temp, sum = 0;
+
+    //set the value to be used to check rest of the square
+    for (col = 0; col < SIZE; col++) {
+        sum += square[0][col];
+        temp = sum;
+    }
+
+    //check if all rows have the same sum
+    if (magic == 0) {
+
+        for (row = 0; row < SIZE; row++) {
+            if (temp == sum) {
+                sum = 0;
+                
+                for (col = 0; col < SIZE; col++) {
+                    sum += square[row][col];
+                }
+
+            } else {
+                magic = 1;
+            }
+        }
+
+    }
+
+    //check if all columns have the same sum
+    if (magic == 0) {
+
+        for (col = 0; col < SIZE; col++) {
+            if (temp == sum) {
+                sum = 0;
+                
+                for (row = 0; row < SIZE; row++) {
+                    sum += square[row][col];
+                }
+                
+            } else {
+                magic = 1;
+            }
+        }
+
+    }
+
+    //check if the sum of diagonal from LEFT to RIGHT have the same value as temp
+    if (magic == 0) {
+        sum = 0;
+
+        for (row = 0; row < SIZE; row++) {
+            sum  += square[row][row];
+        }
+
+        magic = (temp == sum) ? 0 : 1;
+    }
+
+    //check if the sum diagonal from RIGHT to LEFT have the same value as temp
+    if (magic == 0) {
+        sum = 0;
+        col = 2;
+
+        for (row = 0; row < SIZE; row++) {
+            sum  += square[row][col--];
+        }
+
+        magic = (temp == sum) ? 0 : 1;
+    }
+
+    return magic;
+}
