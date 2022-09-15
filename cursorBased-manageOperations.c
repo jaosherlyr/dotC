d0e86a70498035547395f3d889a6d50e18c15e1f
jaosherlyr <70427806+jaosherlyr@users.noreply.github.com>
2022-09-15 16:59:50 +0800

    cursorBasedManagement

---
+++ cursorBased-manageOperations.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define SIZE 10
+typedef struct {
+    char data;
+    int link;
+} Nodetype;
+typedef struct {
+    Nodetype Nodes[SIZE];
+    int avail;
+} VirtualHeap;
+typedef int List;
+void initializeVH(VirtualHeap * heap);
+int allocSpace(VirtualHeap * heap);
+void deallocSpace(VirtualHeap * heap, List L);
+int main() {
+    VirtualHeap VH;
+    List ndx;
+
+    initializeVH(&VH);
+    ndx = allocSpace(&VH); 
+    deallocSpace(&VH, ndx);
+}
+void initializeVH(VirtualHeap * heap) {
+    int ctr;
+
+    for (ctr = 0; ctr < SIZE - 1; ctr++) {
+        heap->Nodes[ctr].link = ctr + 1;
+    }
+
+    heap->Nodes[ctr].link = -1;
+    heap->avail = 0;
+}
+int allocSpace(VirtualHeap * heap) {
+    int temp = -1;
+
+    if (heap->avail != -1) {
+        temp = heap->avail;
+        heap->avail = heap->Nodes[temp].link;
+    }
+
+    return temp;
+}
+void deallocSpace(VirtualHeap * heap, List L) {
+    if (L < SIZE && L != -1) {
+        heap->Nodes[L].link = heap->avail;
+        heap->avail = L;
+    }
+}
