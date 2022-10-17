de3bb4984783d825d6f44de72583deddc74ef750
Sherly Jao <jaosherlyr@gmail.com>
2022-10-17 20:15:47 +0800

    bitwise union

---
+++ bitWise-Union.c
@@ -0,0 +1,33 @@
+//create a union function for bitwise
+#include <stdio.h>
+#include <stdlib.h>
+#define size 10
+typedef int SET[size];
+
+SET * Union(SET A, SET B);
+int main() {
+    SET A = {1, 0, 1, 0, 1, 0, 1, 1, 0, 1};
+    SET B = {0, 1, 0, 0, 1, 0, 0, 0, 1, 0};
+    SET * C;
+
+    C = Union(A, B);
+
+    //display
+    int ctr;
+
+    for (ctr = 0; ctr < size; ctr++) {
+        printf("%d ", (*C)[ctr]);
+    }
+}
+SET * Union(SET A, SET B) {
+	SET * C;
+	int ctr;
+	
+	C = (SET *)malloc(sizeof(SET));
+	
+	for (ctr = 0; ctr < size; ctr++) {
+		(*C)[ctr] = (A[ctr] == 1 || B[ctr] == 1);
+	}
+	
+	return C;
+}
